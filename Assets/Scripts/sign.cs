e49f8d1cb3ab51ed9eb688f8ea2d59e276c5b184
deswinteil <109189977+deswinteil@users.noreply.github.com>
2022-07-19 03:46:20 +0800

    dialog box

---
+++ Assets/Scripts/sign.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sign : MonoBehaviour
+{
+    public GameObject dialogbox;
+    public Text dialogboxtext;
+    public string signtext;
+    private bool playerinsign;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && playerinsign)
+        {
+            Debug.Log("按下e键");
+            dialogboxtext.text = signtext;
+            dialogbox.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("进入范围");
+        playerinsign = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("离开范围");
+        playerinsign = false;
+        dialogbox.SetActive(false);
+    }
+}
