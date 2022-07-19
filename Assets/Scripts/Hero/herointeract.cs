a4675f51f529696175ed27aa721631986fd62874
Zipper-1 <gongzhangpeng42@gmail.com>
2022-07-19 14:58:39 +0800

    持续扣血

--- Assets/Scripts/Hero/herointeract.cs
+++ Assets/Scripts/Hero/herointeract.cs
@@ -6,6 +6,7 @@ public class herointeract : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject muim;
+    private float mHurtTimer = 0;
     void Start()
     {
 
@@ -29,11 +30,29 @@ public class herointeract : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
+        
         if (collider.gameObject.layer == 14 || collider.gameObject.layer == 13)
         {
+            mHurtTimer = 0;
             muim.GetComponent<bloodbarcontrol>().decreasevolume(1f);
             gameObject.GetComponent<HeroMovement>().hurt();
         }
         //gameObject.GetComponent<HeroMovement>().setspeed(-50*((float)gameObject.GetComponent<HeroMovement>().getMoveDirection()));
     }
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == 14 || collider.gameObject.layer == 13)
+        {
+            mHurtTimer += Time.deltaTime;
+            if (mHurtTimer >= 1f)
+            {
+                mHurtTimer = 0;
+                muim.GetComponent<bloodbarcontrol>().decreasevolume(1f);
+                gameObject.GetComponent<HeroMovement>().hurt();
+            }
+            
+        }
+        //gameObject.GetComponent<HeroMovement>().setspeed(-50*((float)gameObject.GetComponent<HeroMovement>().getMoveDirection()));
+    }
+
 }
