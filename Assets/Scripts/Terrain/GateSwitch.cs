6b843402c00e9739e46f14e5d53d24d85cec4732
Zipper-1 <gongzhangpeng42@gmail.com>
2022-07-19 21:32:44 +0800

    Update Gate

---
+++ Assets/Scripts/Terrain/GateSwitch.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            foreach(Transform i in transform)
+            {
+                i.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+}
