39d76abdb6d131618d5d0c86ec48d48d5de95592
Zipper-1 <gongzhangpeng42@gmail.com>
2022-07-21 18:36:23 +0800

    11111

--- Assets/Scripts/Scene/ScenesManager.cs
+++ Assets/Scripts/Scene/ScenesManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.LogWarning(SceneManager.GetActiveScene().name+"onload");
+            GameObject hero;
+            if (hero = GameObject.Find("hero"))
+            {
+                hero.GetComponent<HeroBehavior>().setRespawnPoint(transform.position);
+                hero.transform.position = transform.position;
+            }
     }
 
     // Update is called once per frame
