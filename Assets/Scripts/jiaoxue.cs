ae30b15c6204f83b6de01d69793bffddfffa68df
RainSun0322 <47513742+RainSun0322@users.noreply.github.com>
2022-07-22 02:18:34 +0800

    Jiaoxue update

--- Assets/Scripts/jiaoxue.cs
+++ Assets/Scripts/jiaoxue.cs
@@ -7,7 +7,7 @@ public class jiaoxue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Renderer>().enabled = true;
+        gameObject.GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
