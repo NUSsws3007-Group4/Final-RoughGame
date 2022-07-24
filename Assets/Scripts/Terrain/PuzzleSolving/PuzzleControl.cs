1c28adbf963e4d279339efc87bf7cdd49b64692c
Zipper-1 <gongzhangpeng42@gmail.com>
2022-07-24 10:54:57 +0800

    Update PuzzleControl.cs

--- Assets/Scripts/Terrain/PuzzleSolving/PuzzleControl.cs
+++ Assets/Scripts/Terrain/PuzzleSolving/PuzzleControl.cs
@@ -25,7 +25,10 @@ public class PuzzleControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (Transform i in transform)
+        {
+            i.gameObject.SetActive(puzzleEnabled);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
