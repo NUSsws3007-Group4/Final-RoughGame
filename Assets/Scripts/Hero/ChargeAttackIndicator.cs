4a6a99636054e021d597f3314c75da1b996fa714
SirinSchariac <106457519+SirinSchariac@users.noreply.github.com>
2022-07-25 22:26:39 +0800

    update charge attack script

--- Assets/Scripts/Hero/ChargeAttackIndicator.cs
+++ Assets/Scripts/Hero/ChargeAttackIndicator.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackIndicatorBehavior : MonoBehaviour
-{  
+public class ChargeAttackIndicator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int extraATKFromCharge = 0;
+
     protected const float OriginCD = 0.6f;//初始设定CD
     protected float attackTimer = 0f;
     protected float normalAttackColdDown;
     void Start()
     {
         attackTimer = 0f;       
-        gameObject.transform.parent.GetComponent<HeroAttackHurt>().hurt = 25;
         normalAttackColdDown = OriginCD * gameObject.transform.parent.GetComponent<HeroAttackHurt>().coldDownCoef;
     }
 
     void Update()
     {
+        gameObject.transform.parent.GetComponent<HeroAttackHurt>().hurt = 25 + extraATKFromCharge;//融合蓄力攻击基数
+
         attackTimer += Time.deltaTime;
         if(attackTimer >= normalAttackColdDown)
         {
@@ -24,6 +28,12 @@ public class AttackIndicatorBehavior : MonoBehaviour
         }
         
     }
+
+    public void ExtraATK(int _damage)//获取蓄力的额外攻击力
+    {
+        extraATKFromCharge = _damage;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
