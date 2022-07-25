130898a050c7cc06b8082370e6c5db5dad02c561
RainSun0322 <47513742+RainSun0322@users.noreply.github.com>
2022-07-25 11:37:22 +0800

    Portal update

--- Assets/Scripts/Terrain/Protal.cs
+++ Assets/Scripts/Terrain/Protal.cs
@@ -9,21 +9,43 @@ public class Protal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer==LayerMask.NameToLayer("Player"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            Debug.LogWarning("Loading: " + nextLevel);
-            DontDestroyOnLoad(GameObject.Find("KeepInTransfer"));
-            SceneManager.LoadScene(nextLevel);
+            transform.GetChild(0).GetComponent<Renderer>().enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.LogWarning("Loading: " + nextLevel);
+                DontDestroyOnLoad(GameObject.Find("KeepInTransfer"));
+                SceneManager.LoadScene(nextLevel);
+            }
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.LogWarning("Loading: " + nextLevel);
+                DontDestroyOnLoad(GameObject.Find("KeepInTransfer"));
+                SceneManager.LoadScene(nextLevel);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.GetChild(0).GetComponent<Renderer>().enabled = false;
     }
 }
